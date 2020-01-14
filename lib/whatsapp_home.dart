592cdad498461fb65c15f1190be2ece97eaf7c90
Kennedy <kennedymessias.vb@gmail.com>
2020-01-14 19:42:40 -0300

    initial page

--- lib/whatsapp_home.dart
+++ lib/whatsapp_home.dart
@@ -7,15 +7,45 @@ class WhatsappHome extends StatefulWidget {
   _WhatsappHomeState createState() => _WhatsappHomeState();
 }
 
-class _WhatsappHomeState extends State<WhatsappHome> with SingleTickerProviderStateMixin {
+class _WhatsappHomeState extends State<WhatsappHome>
+    with SingleTickerProviderStateMixin {
   TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, initialIndex: 1, length: 4);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: new AppBar(
-         title: new Text("WhatsApp"),
-         elevation: 0.7,
-       ),
+      appBar: new AppBar(
+        title: new Text("WhatsApp"),
+        elevation: 0.8,
+        bottom: new TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(
+              icon: new Icon(Icons.camera_alt),
+            ),
+            new Tab(
+              text: "CONVERSAS",
+            ),
+            new Tab(
+              text: "STATUS",
+            ),
+            new Tab(
+              text: "LIGAÇÕES",
+            )
+          ],
+        ),
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: <Widget>[],
+      ),
     );
   }
 }
